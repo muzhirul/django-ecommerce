8cadacd7f96018d3026923dfeb3f8e453cd14378
muzhirul <muzhirulislam@gmail.com>
2022-04-20 11:10:42 +0600

    working with reset password

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,5 +10,9 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('', views.dashboard, name='dashboard'),
     path('activate/<uidb64>/<token>', views.activate, name='activate'),
+    path('resetpassword_validate/<uidb64>/<token>', views.resetpassword_validate, name='resetpassword_validate'),
+    
+    path('forgotPassword', views.forgotPassword, name='forgotPassword'), 
+    path('resetPassword', views.resetPassword, name='resetPassword'), 
     
 ]
