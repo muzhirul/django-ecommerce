6481664bf8e1cafea7f448ae5ddc8443bebf1796
muzhirul <muzhirulislam@gmail.com>
2022-04-21 16:14:26 +0600

    checkout functions

--- greatcart/urls.py
+++ greatcart/urls.py
@@ -27,6 +27,9 @@ urlpatterns = [
     path('store/', include('store.urls')),
     path('cart/', include('carts.urls')),
     path('accounts/', include('accounts.urls')),
+    
+    #Orders
+    path('orders/', include('orders.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=MEDIA_ROOT)
---
+++ orders/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from orders.models import Payment, Order, OrderProduct
+# Register your models here.
+
+admin.site.register(Payment)
+admin.site.register(Order)
+admin.site.register(OrderProduct)
